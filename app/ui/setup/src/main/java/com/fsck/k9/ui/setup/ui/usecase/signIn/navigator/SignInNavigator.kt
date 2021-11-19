d0edddc94a06d273a390f5d8489e7cb491bd097f
jstarczewski <jan.a.starczewski@gmail.com>
2021-11-19 14:23:45 +0100

    Create navigator with injection of base nav impl to outside of module

---
+++ app/ui/setup/src/main/java/com/fsck/k9/ui/setup/ui/usecase/signIn/navigator/SignInNavigator.kt
@@ -0,0 +1,8 @@
+package com.fsck.k9.ui.setup.ui.usecase.signIn.navigator
+
+import com.fsck.k9.ui.setup.ui.navigation.navigator.Navigator
+
+interface SignInNavigator : Navigator {
+
+    fun moveToCredentials()
+}
