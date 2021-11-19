62c3e32c80fc85346f7789d6bc4425b33bca6f04
jstarczewski <jan.a.starczewski@gmail.com>
2021-11-19 14:21:53 +0100

    Define colors themes and types for sake of poc

---
+++ app/ui/setup/src/main/java/com/fsck/k9/ui/setup/ui/theme/Colors.kt
@@ -0,0 +1,10 @@
+package com.fsck.k9.ui.setup.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PaleRed = Color(0xffd85252)
+val Hint = Color(0xFF8A94A6)
+val WhitePure = Color.White
+val GreyLight = Color(0xFFEBEBEB)
+val BlackPure = Color.Black
+val GreyDark = Color(0xFF545A5E)
