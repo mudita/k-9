9d95599d2159a6414b9e6de6412f4fc33f96ec30
jstarczewski <jan.a.starczewski@gmail.com>
2021-11-22 13:23:34 +0100

    Use extension to retrive param from passed invocation

--- app/ui/latest/src/main/java/com/mudita/mail/ui/di/NavigationModule.kt
+++ app/ui/latest/src/main/java/com/mudita/mail/ui/di/NavigationModule.kt
@@ -1,5 +1,6 @@
 package com.mudita.mail.ui.di
 
+import com.mudita.mail.ui.extension.getFrom
 import com.mudita.mail.ui.usecase.signIn.navigator.SignInNavigator
 import com.mudita.mail.ui.usecase.signIn.navigator.SignInScreenNavigatorImpl
 import org.koin.dsl.module
@@ -8,8 +9,7 @@ val composeNavigationModule = module {
 
     factory<SignInNavigator> { params ->
         SignInScreenNavigatorImpl(
-            context = params.get(),
-            // injection from shared koin module of interface impl
+            context = getFrom(params),
             toSetupAccountNavigator = get()
         )
     }
