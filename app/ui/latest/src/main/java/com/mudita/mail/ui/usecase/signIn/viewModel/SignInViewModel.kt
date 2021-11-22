9ebb35a8ef8e4d6f6374dad5afb984b9ac23aa04
jstarczewski <jan.a.starczewski@gmail.com>
2021-11-22 18:23:44 +0100

    Implement basic sign in view model

--- app/ui/latest/src/main/java/com/mudita/mail/ui/usecase/signIn/viewModel/SignInViewModel.kt
+++ app/ui/latest/src/main/java/com/mudita/mail/ui/usecase/signIn/viewModel/SignInViewModel.kt
@@ -1,16 +1,38 @@
 package com.mudita.mail.ui.usecase.signIn.viewModel
 
 import androidx.lifecycle.ViewModel
+import com.mudita.mail.interactor.signIn.SignInInteractor
+import com.mudita.mail.repository.providers.model.ProviderTile
+import com.mudita.mail.repository.providers.model.ProviderType
 import com.mudita.mail.ui.usecase.signIn.navigator.SignInNavigator
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+
+data class UiState(
+    val providers: List<ProviderTile> = emptyList()
+)
 
 class SignInViewModel(
-    // Other dependencies like interactor aggregating calls
-    // to other services repositories etc
-    // val interactor: SignInInteractor
+    interactor: SignInInteractor,
     private val navigator: SignInNavigator
 ) : ViewModel() {
 
-    fun gmailSelected() {
-        navigator.moveToCredentials()
+    private val _uiState = MutableStateFlow(UiState())
+    val uiState: StateFlow<UiState> = _uiState
+
+    init {
+        interactor.getProviders().let { providers ->
+            _uiState.update { it.copy(providers = providers) }
+        }
+    }
+
+    fun selectProvider(providerType: ProviderType) {
+        when (providerType) {
+            ProviderType.GMAIL,
+            ProviderType.ICLOUD,
+            ProviderType.OUTLOOK,
+            ProviderType.MANUAL -> navigator.moveToCredentials()
+        }
     }
 }
