2fd7b042f9429c64953c260c1805c5f1ec1c89e0
jstarczewski <jan.a.starczewski@gmail.com>
2021-11-22 18:22:42 +0100

    Enhance extension to get from params

--- app/ui/latest/src/main/java/com/mudita/mail/ui/extension/GetWith.kt
+++ app/ui/latest/src/main/java/com/mudita/mail/ui/extension/GetWith.kt
@@ -4,5 +4,5 @@ import org.koin.core.parameter.ParametersHolder
 import org.koin.core.parameter.parametersOf
 import org.koin.core.scope.Scope
 
-inline fun <reified T : Any> Scope.getFrom(params: ParametersHolder): T =
+inline fun <reified T : Any> Scope.getWith(params: ParametersHolder): T =
     params.getOrNull() ?: get { parametersOf(*params.values.toTypedArray()) }
