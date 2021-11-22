37601865782b44403af1c70e136c5027aebba409
jstarczewski <jan.a.starczewski@gmail.com>
2021-11-22 18:24:00 +0100

    add modules to composeU

--- app/ui/latest/src/main/java/com/mudita/mail/ComposeUiModules.kt
+++ app/ui/latest/src/main/java/com/mudita/mail/ComposeUiModules.kt
@@ -2,8 +2,16 @@ package com.mudita.mail
 
 import com.mudita.mail.ui.di.composeNavigationModule
 import com.mudita.mail.ui.di.composeViewModelModule
+import com.mudita.mail.ui.di.interactorModule
+import com.mudita.mail.ui.di.repositoryModule
+import com.mudita.mail.ui.util.imageResolverModule
+import com.mudita.mail.ui.util.stringKey.stringResolverModule
 
 val composeUiModules = listOf(
     composeNavigationModule,
-    composeViewModelModule
+    composeViewModelModule,
+    imageResolverModule,
+    stringResolverModule,
+    interactorModule,
+    repositoryModule
 )
