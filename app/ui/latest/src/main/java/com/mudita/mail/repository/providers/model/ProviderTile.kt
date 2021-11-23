8de7f51e8eb419698b1c7548d159c0e89523bb08
jstarczewski <jan.a.starczewski@gmail.com>
2021-11-23 09:32:00 +0100

    Change model

--- app/ui/latest/src/main/java/com/mudita/mail/repository/providers/model/ProviderTile.kt
+++ app/ui/latest/src/main/java/com/mudita/mail/repository/providers/model/ProviderTile.kt
@@ -1,7 +1,9 @@
 package com.mudita.mail.repository.providers.model
 
+import com.mudita.mail.ui.util.stringKey.StringKey
+
 data class ProviderTile(
-    val name: String,
-    val description: String,
+    val nameKey: StringKey,
+    val descriptionKey: StringKey,
     val type: ProviderType
 )
