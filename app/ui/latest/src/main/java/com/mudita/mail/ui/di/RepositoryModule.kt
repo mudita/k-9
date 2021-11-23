c6d95f9725249db061b6a0518858b105ed396153
jstarczewski <jan.a.starczewski@gmail.com>
2021-11-23 09:32:34 +0100

    Change di binding in repository module

--- app/ui/latest/src/main/java/com/mudita/mail/ui/di/RepositoryModule.kt
+++ app/ui/latest/src/main/java/com/mudita/mail/ui/di/RepositoryModule.kt
@@ -1,10 +1,10 @@
 package com.mudita.mail.ui.di
 
 import com.mudita.mail.repository.providers.ProvidersRepository
-import com.mudita.mail.repository.providers.ResBasedProvidersRepository
+import com.mudita.mail.repository.providers.KeyBasedProvidersRepository
 import org.koin.dsl.module
 
 val repositoryModule = module {
 
-    factory<ProvidersRepository> { ResBasedProvidersRepository(get()) }
+    factory<ProvidersRepository> { KeyBasedProvidersRepository() }
 }
