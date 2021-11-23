a4f9ab3eb9590698bfc0b64578970bb84a5b5285
jstarczewski <jan.a.starczewski@gmail.com>
2021-11-23 19:53:30 +0100

    Get rid of useless coments

--- app/ui/base/src/main/java/com/fsck/k9/ui/base/navigation/ToSetupAccountNavigator.kt
+++ app/ui/base/src/main/java/com/fsck/k9/ui/base/navigation/ToSetupAccountNavigator.kt
@@ -2,8 +2,6 @@ package com.fsck.k9.ui.base.navigation
 
 import android.content.Context
 
-// Here the trick we define an interface in base module responsible for
-// between other not related modules navigation
 interface ToSetupAccountNavigator {
 
     fun moveToSetupAccount(context: Context)
