cf96854b0af1c2d96028dc95b1377267fe550c2d
jstarczewski <jan.a.starczewski@gmail.com>
2021-11-25 13:51:47 +0100

    Inject auth relay to extend activity

--- app/ui/latest/src/main/java/com/mudita/mail/ui/SignInActivity.kt
+++ app/ui/latest/src/main/java/com/mudita/mail/ui/SignInActivity.kt
@@ -2,10 +2,10 @@ package com.mudita.mail.ui
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import com.fsck.k9.ui.base.K9Activity
+import com.fsck.k9.DI
 import com.mudita.mail.ui.usecase.MuditaMailApp
 
-class SignInActivity : K9Activity() {
+class SignInActivity : BaseAuthActivity(DI.get()) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
