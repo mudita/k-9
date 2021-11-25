e629d53ef99a92779abb31fe0693f40e6243c49c
jstarczewski <jan.a.starczewski@gmail.com>
2021-11-25 13:51:35 +0100

    Reference all DI modules

--- app/ui/latest/src/main/java/com/mudita/mail/ComposeUiModules.kt
+++ app/ui/latest/src/main/java/com/mudita/mail/ComposeUiModules.kt
@@ -1,13 +1,17 @@
 package com.mudita.mail
 
-import com.mudita.mail.ui.di.composeNavigationModule
-import com.mudita.mail.ui.di.composeViewModelModule
-import com.mudita.mail.ui.di.interactorModule
-import com.mudita.mail.ui.di.repositoryModule
+import com.mudita.mail.di.composeNavigationModule
+import com.mudita.mail.di.composeViewModelModule
+import com.mudita.mail.di.interactorModule
+import com.mudita.mail.di.relayModule
+import com.mudita.mail.di.repositoryModule
+import com.mudita.mail.di.serviceModule
 
 val composeUiModules = listOf(
     composeNavigationModule,
     composeViewModelModule,
     interactorModule,
-    repositoryModule
+    repositoryModule,
+    serviceModule,
+    relayModule
 )
