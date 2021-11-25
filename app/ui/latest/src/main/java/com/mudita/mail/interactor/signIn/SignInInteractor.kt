4a9d8dc3628752f76283ed584ef1bd35384b13e1
jstarczewski <jan.a.starczewski@gmail.com>
2021-11-25 13:50:56 +0100

    Add functions to sign in interactor

--- app/ui/latest/src/main/java/com/mudita/mail/interactor/signIn/SignInInteractor.kt
+++ app/ui/latest/src/main/java/com/mudita/mail/interactor/signIn/SignInInteractor.kt
@@ -1,8 +1,16 @@
 package com.mudita.mail.interactor.signIn
 
 import com.mudita.mail.repository.providers.model.ProviderTile
+import com.mudita.mail.repository.providers.model.ProviderType
+import com.mudita.mail.service.authConfig.AuthConfig
 
 interface SignInInteractor {
 
     fun getProviders(): List<ProviderTile>
+
+    fun getProviderAuthConfig(providerType: ProviderType): AuthConfig?
+
+    suspend fun startAuthProcess(authConfig: AuthConfig)
+
+    suspend fun processAuthData() : String
 }
