9aa26553340aa76c1cfffba073cc31a76d424564
jstarczewski <jan.a.starczewski@gmail.com>
2021-11-29 20:06:08 +0100

    Rename param in external navigator

--- app/ui/legacy/src/main/java/com/fsck/k9/navigation/ToSetupAccountNavigatorImpl.kt
+++ app/ui/legacy/src/main/java/com/fsck/k9/navigation/ToSetupAccountNavigatorImpl.kt
@@ -1,13 +1,12 @@
 package com.fsck.k9.navigation
 
 import android.content.Context
-import android.content.Intent
 import com.fsck.k9.activity.setup.AccountSetupBasics
 import com.fsck.k9.ui.base.navigation.ToSetupAccountNavigator
 
 class ToSetupAccountNavigatorImpl : ToSetupAccountNavigator {
 
-    override fun moveToSetupAccount(context: Context) {
-        context.run { startActivity(Intent(this, AccountSetupBasics::class.java)) }
+    override fun moveToSetupAccount(context: Context, username: String) {
+        context.run { AccountSetupBasics.startActivityWithEmailSet(this, username) }
     }
 }
