cd93110ccaa77740d79c068d36b56ee9462420e7
jstarczewski <jan.a.starczewski@gmail.com>
2021-11-29 20:02:12 +0100

    Refactor sign in interactor to make it responsible only for navigation with selected provider

--- app/ui/latest/src/main/java/com/mudita/mail/interactor/signIn/SignInInteractor.kt
+++ app/ui/latest/src/main/java/com/mudita/mail/interactor/signIn/SignInInteractor.kt
@@ -1,16 +1,8 @@
 package com.mudita.mail.interactor.signIn
 
 import com.mudita.mail.repository.providers.model.ProviderTile
-import com.mudita.mail.repository.providers.model.ProviderType
-import com.mudita.mail.service.authConfig.AuthConfig
 
 interface SignInInteractor {
 
     fun getProviders(): List<ProviderTile>
-
-    fun getProviderAuthConfig(providerType: ProviderType): AuthConfig?
-
-    suspend fun startAuthProcess(authConfig: AuthConfig)
-
-    suspend fun processAuthData() : String
 }
--- app/ui/latest/src/main/java/com/mudita/mail/interactor/signIn/SignInInteractorImpl.kt
+++ app/ui/latest/src/main/java/com/mudita/mail/interactor/signIn/SignInInteractorImpl.kt
@@ -1,24 +1,10 @@
 package com.mudita.mail.interactor.signIn
 
-import com.mudita.mail.relay.auth.AuthRelay
 import com.mudita.mail.repository.providers.ProvidersRepository
-import com.mudita.mail.repository.providers.model.ProviderType
-import com.mudita.mail.service.authConfig.AuthConfig
-import com.mudita.mail.service.authConfig.AuthConfigService
 
 class SignInInteractorImpl(
     private val providersRepository: ProvidersRepository,
-    private val authConfigService: AuthConfigService,
-    private val authRelay: AuthRelay
 ) : SignInInteractor {
 
     override fun getProviders() = providersRepository.getProviders()
-
-    override fun getProviderAuthConfig(providerType: ProviderType) =
-        authConfigService.getAuthConfigForProviderType(providerType)
-
-    override suspend fun startAuthProcess(authConfig: AuthConfig) =
-        authRelay.offerInput(authConfig)
-
-    override suspend fun processAuthData() = authRelay.requestResult()
 }
