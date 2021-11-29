8d7a71c0f339b6e71897504b82274c9a971f5606
jstarczewski <jan.a.starczewski@gmail.com>
2021-11-29 19:59:16 +0100

    Get rid of BaseAuthActivit

--- app/ui/latest/src/main/java/com/mudita/mail/ui/SignInActivity.kt
+++ app/ui/latest/src/main/java/com/mudita/mail/ui/SignInActivity.kt
@@ -2,10 +2,10 @@ package com.mudita.mail.ui
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import com.fsck.k9.DI
+import com.fsck.k9.ui.base.K9Activity
 import com.mudita.mail.ui.usecase.MuditaMailApp
 
-class SignInActivity : BaseAuthActivity(DI.get()) {
+class SignInActivity : K9Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
