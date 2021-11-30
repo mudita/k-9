d7106b5aa3d1a6e9f6119dd5955966e5e1f07b64
jstarczewski <jan.a.starczewski@gmail.com>
2021-11-30 14:06:20 +0100

    Adjust interface impl

--- app/autodiscovery/thunderbird/src/main/java/com/fsck/k9/autodiscovery/thunderbird/ThunderbirdDiscovery.kt
+++ app/autodiscovery/thunderbird/src/main/java/com/fsck/k9/autodiscovery/thunderbird/ThunderbirdDiscovery.kt
@@ -3,13 +3,14 @@ package com.fsck.k9.autodiscovery.thunderbird
 import com.fsck.k9.autodiscovery.api.ConnectionSettingsDiscovery
 import com.fsck.k9.autodiscovery.api.DiscoveryResults
 import com.fsck.k9.autodiscovery.api.DiscoveryTarget
+import com.fsck.k9.mail.AuthType
 
 class ThunderbirdDiscovery(
     private val fetcher: ThunderbirdAutoconfigFetcher,
     private val parser: ThunderbirdAutoconfigParser
 ) : ConnectionSettingsDiscovery {
 
-    override fun discover(email: String, target: DiscoveryTarget): DiscoveryResults? {
+    override fun discover(email: String, target: DiscoveryTarget, predefinedAuthType: AuthType?): DiscoveryResults? {
         val autoconfigInputStream = fetcher.fetchAutoconfigFile(email) ?: return null
 
         return autoconfigInputStream.use {
