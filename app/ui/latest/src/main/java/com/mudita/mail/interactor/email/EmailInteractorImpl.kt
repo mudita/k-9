715c4b80d59d856c01bc567c75b3250236a1fdb4
jstarczewski <jan.a.starczewski@gmail.com>
2021-11-30 19:51:56 +0100

    Run linter

--- app/ui/latest/src/main/java/com/mudita/mail/interactor/email/EmailInteractorImpl.kt
+++ app/ui/latest/src/main/java/com/mudita/mail/interactor/email/EmailInteractorImpl.kt
@@ -2,10 +2,10 @@ package com.mudita.mail.interactor.email
 
 import com.mudita.mail.repository.auth.config.AuthConfig
 import com.mudita.mail.repository.providers.model.ProviderType
-import com.mudita.mail.service.auth.config.AuthConfigService
-import com.mudita.mail.service.auth.AuthService
 import com.mudita.mail.service.auth.AuthRequestData
 import com.mudita.mail.service.auth.AuthResponseData
+import com.mudita.mail.service.auth.AuthService
+import com.mudita.mail.service.auth.config.AuthConfigService
 
 class EmailInteractorImpl(
     private val authConfigService: AuthConfigService,
--- app/ui/latest/src/main/java/com/mudita/mail/service/api/client/ApiClientService.kt
+++ app/ui/latest/src/main/java/com/mudita/mail/service/api/client/ApiClientService.kt
@@ -4,7 +4,7 @@ import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
 
-//TODO refactor to be based on an interface
+// TODO refactor to be based on an interface
 class ApiClientService(
     val client: HttpClient
 ) {
