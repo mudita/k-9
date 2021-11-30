8b787f0e7b640ef19c8a7bc924abf2c1a1029899
jstarczewski <jan.a.starczewski@gmail.com>
2021-11-30 19:35:38 +0100

    Create super simple api client service to make http calls

---
+++ app/ui/latest/src/main/java/com/mudita/mail/service/api/client/ApiClientService.kt
@@ -0,0 +1,16 @@
+package com.mudita.mail.service.api.client
+
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+
+//TODO refactor to be based on an interface
+class ApiClientService(
+    val client: HttpClient
+) {
+
+    suspend inline fun <reified T> get(
+        path: String,
+        parameters: Map<String, Any>
+    ): T = client.get(path) { parameters.forEach { parameter(it.key, it.value) } }
+}
