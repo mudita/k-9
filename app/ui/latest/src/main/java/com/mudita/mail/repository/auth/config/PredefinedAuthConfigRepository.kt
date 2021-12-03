a4f5f8f0896dbaccf96976b0e98d1f3febf4fa1d
jstarczewski <jan.a.starczewski@gmail.com>
2021-12-03 14:18:23 +0100

    add outlook config to repo

--- app/ui/latest/src/main/java/com/mudita/mail/repository/auth/config/PredefinedAuthConfigRepository.kt
+++ app/ui/latest/src/main/java/com/mudita/mail/repository/auth/config/PredefinedAuthConfigRepository.kt
@@ -14,6 +14,20 @@ class PredefinedAuthConfigRepository(context: Context) : AuthConfigRepository {
             responseType = ResponseType.CODE,
             authEndpoint = "https://accounts.google.com/o/oauth2/auth",
             tokenEndpoint = "https://oauth2.googleapis.com/token"
+        ),
+        AuthConfig(
+            clientName = "OUTLOOK",
+            clientId = context.getString(R.string.outlook_client_id),
+            redirectUrl = context.getString(R.string.outlook_redirect_url),
+            scopes = listOf(
+                "https://outlook.office.com/IMAP.AccessAsUser.All",
+                "https://outlook.office.com/POP.AccessAsUser.All",
+                "https://outlook.office.com/SMTP.Send",
+                "offline_access"
+            ),
+            responseType = ResponseType.CODE,
+            authEndpoint = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize",
+            tokenEndpoint = "https://login.microsoftonline.com/common/oauth2/v2.0/token"
         )
     )
 
