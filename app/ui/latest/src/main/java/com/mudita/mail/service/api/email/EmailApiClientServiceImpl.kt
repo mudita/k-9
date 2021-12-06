56ec28fde0e2108a124db29ba59d35647b1a5fc9
jstarczewski <jan.a.starczewski@gmail.com>
2021-12-06 15:43:27 +0100

    Use strategies in getting emaol

--- app/ui/latest/src/main/java/com/mudita/mail/service/api/email/EmailApiClientServiceImpl.kt
+++ app/ui/latest/src/main/java/com/mudita/mail/service/api/email/EmailApiClientServiceImpl.kt
@@ -1,19 +1,26 @@
 package com.mudita.mail.service.api.email
 
+import com.mudita.mail.repository.providers.model.ProviderType
 import com.mudita.mail.service.api.client.ApiClientService
+import com.mudita.mail.service.api.email.strategy.EmailApiContext
+import com.mudita.mail.service.api.email.strategy.google.GoogleCloudEmailApiServiceStrategy
+import com.mudita.mail.service.api.email.strategy.microsoft.MicrosoftEmailApiServiceStrategy
 
 class EmailApiClientServiceImpl(
+    private val emailApiContext: EmailApiContext,
     private val apiClientService: ApiClientService
 ) : EmailApiClientService {
 
-    override suspend fun getEmail(token: String) =
-        apiClientService.get<UserInfo>(
-            "https://www.googleapis.com/oauth2/v2/userinfo",
-            parameters = mapOf(ACCESS_TOKEN to token)
-        ).map { it.email }
-
-    companion object {
-
-        private const val ACCESS_TOKEN = "access_token"
+    override suspend fun getEmail(
+        providerType: ProviderType,
+        token: String
+    ): Result<String> {
+        when (providerType) {
+            ProviderType.GMAIL -> emailApiContext.setStrategy(GoogleCloudEmailApiServiceStrategy(apiClientService))
+            ProviderType.OUTLOOK -> emailApiContext.setStrategy(MicrosoftEmailApiServiceStrategy(apiClientService))
+            ProviderType.ICLOUD,
+            ProviderType.MANUAL -> throw IllegalArgumentException()
+        }
+        return emailApiContext.executeStrategy(token)
     }
 }
