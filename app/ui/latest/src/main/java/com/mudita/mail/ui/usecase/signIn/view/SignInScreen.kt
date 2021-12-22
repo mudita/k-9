eacce72d5ce3382c5cf95dbfc19bdce69407643f
jstarczewski <jan.a.starczewski@gmail.com>
2021-12-22 12:19:15 +0100

    delete useless imports

--- app/ui/latest/src/main/java/com/mudita/mail/ui/usecase/signIn/view/SignInScreen.kt
+++ app/ui/latest/src/main/java/com/mudita/mail/ui/usecase/signIn/view/SignInScreen.kt
@@ -4,21 +4,13 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.ScrollState
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Divider
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ModalBottomSheetState
@@ -31,7 +23,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -188,30 +179,6 @@ fun AvailableProviders(
                 }
             }
         }
-    }
-}
-
-@Composable
-fun TopHideIndicatorBottomSheet(content: @Composable () -> Unit) {
-    Column(
-        modifier = Modifier
-            .clip(RoundedCornerShape(topStart = 25.dp, topEnd = 25.dp))
-            .fillMaxWidth()
-            .verticalScroll(ScrollState(0))
-            .background(MaterialTheme.colors.background),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Divider(
-            modifier = Modifier
-                .padding(top = 8.dp, bottom = 16.dp)
-                .width(60.dp)
-                .height(4.dp)
-                .clip(RoundedCornerShape(4.dp))
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        content()
-        Spacer(modifier = Modifier.height(32.dp))
     }
 }
 
