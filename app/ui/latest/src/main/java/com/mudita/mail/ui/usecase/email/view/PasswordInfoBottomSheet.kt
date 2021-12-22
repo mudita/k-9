79e489e5af84df4c57eb14171e6b9c24c90e6424
jstarczewski <jan.a.starczewski@gmail.com>
2021-12-22 12:16:32 +0100

    Create password info bottom sheet

---
+++ app/ui/latest/src/main/java/com/mudita/mail/ui/usecase/email/view/PasswordInfoBottomSheet.kt
@@ -0,0 +1,59 @@
+package com.mudita.mail.ui.usecase.email.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mudita.mail.ui.theme.BlackPure
+import com.mudita.mail.ui.theme.MuditaTheme
+import com.mudita.mail.ui.theme.WhitePure
+import com.mudita.mail.ui.usecase.signIn.view.TopHideIndicatorBottomSheet
+
+@Composable
+fun PasswordInfoBootmSheet(
+    onTapAction: () -> Unit
+) {
+    TopHideIndicatorBottomSheet {
+        Text(
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Center,
+            text = "Kliknij w poniższy przycisk aby przejść do generacji jednorazowego hasła w bezpiecznej przeglądarce. Po zakończeniu procesu przekopiuj hasło do formularza logowania"
+        )
+        Column(
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp, top = 16.dp)
+                .clip(RoundedCornerShape(4.dp))
+                .clickable { onTapAction() }
+                .background(color = BlackPure)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                text = "Wygeneruj hasło",
+                color = WhitePure,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PasswordInfoPreview() {
+    MuditaTheme {
+        PasswordInfoBootmSheet {}
+    }
+}
