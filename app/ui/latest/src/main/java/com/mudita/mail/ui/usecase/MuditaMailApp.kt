6249943695e6e8b4e147082f1fa48256de7e9ca7
jstarczewski <jan.a.starczewski@gmail.com>
2022-01-27 14:33:49 +0100

    Implement passing start destination

--- app/ui/latest/src/main/java/com/mudita/mail/ui/usecase/MuditaMailApp.kt
+++ app/ui/latest/src/main/java/com/mudita/mail/ui/usecase/MuditaMailApp.kt
@@ -4,18 +4,24 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.rememberNavController
+import com.mudita.mail.ui.navigation.Destination
 import com.mudita.mail.ui.navigation.MuditaMailNavGraph
 import com.mudita.mail.ui.theme.MuditaTheme
 
 @Composable
-fun MuditaMailApp() {
+fun MuditaMailApp(
+    startDestination: Destination
+) {
     MuditaTheme {
         val navController = rememberNavController()
         val scaffoldState = rememberScaffoldState()
         Scaffold(
             scaffoldState = scaffoldState
         ) {
-            MuditaMailNavGraph(navController)
+            MuditaMailNavGraph(
+                startDestination,
+                navController
+            )
         }
     }
 }
